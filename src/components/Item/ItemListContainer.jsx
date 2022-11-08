9e2c230413aa16559db1c8b398debb54a14fda7d
Priscila Pace <priscila.d.pace@gmail.com>
2022-11-08 18:25:24 -0300

    uso data array

--- src/components/Item/ItemListContainer.jsx
+++ src/components/Item/ItemListContainer.jsx
@@ -1,5 +1,6 @@
 import Item from './Item';
-function ItemListContainer(props){
+import Products from '../../data.js/data';
+function ItemListContainer(props) {
 
     return (
         <div>
@@ -8,18 +9,14 @@ function ItemListContainer(props){
             </div>
             <h3 className='listTittle'>Mis Productos</h3>
             <div className='cuerpoCards'>
-            <Item img="./img/mat.jpg" tittle="Mat" price={5000} 
-            descripcion="Mat de Yoga Ultraresistente. Material ecológico TPE." stock={20}/>
-            <Item img="./img/cubo.jpg" tittle="Cubo" price={1000} 
-            descripcion="Cubo liviano ideal para mejorar posturas." stock={50}/>
-            <Item img="./img/zafu.jpg" tittle="Zafu" price={2500} 
-            descripcion="Zafu de meditación. Relleno de semillas, tela lavable." stock={12}/>
-            <Item img="./img/cinto.jpg" tittle="Cinto" price={900} 
-            descripcion="Cinto resistente de 2 metros. Mejora tus asanas." stock={25}/>
-            <Item img="./img/esfera.jpg" tittle="Esfera" price={3500} 
-            descripcion="Esfera para trabajar distintos planos del cuerpo de forma segura." stock={8}/>
-            <Item img="./img/kit.jpg" tittle="Kit Meditación" price={2800} 
-            descripcion="Kit de almohadilla para ojos, almohada cervical y almohadilla extra. Rellenas de semillas y lavanda." stock={10}/>
+                {Products.map((product) => {
+                    return (
+                        <Item img={product.img} tittle={product.nombre} price={product.precio}
+                            descripcion={product.descripcion} stock={product.stock} />
+                    )
+                }
+                )}
+
             </div>
         </div>
     );
---
+++ src/data.js/data.js
@@ -0,0 +1,64 @@
+const products = 
+[{
+    "id": 1,
+    "idCategoria": "práctica",
+    "nombre": "Mat Ultra Resistente",
+    "descripcion": "Colchoneta para Yoga de TPE, 6mm, ecológico y duradero. No desgrana.",
+    "precio": 10000,
+    "stock": 30,
+    "img": "img/mat.jpg"
+
+},
+{
+    "id": 2,
+    "idCategoria": "práctica",
+    "nombre": "Cubos",
+    "descripcion": "Ladrillo de goma eva, tamaño clásico. Viene x 2 unidades para complementar las posturas",
+    "precio": 2000,
+    "stock": 15,
+    "img": "img/cubo.jpg"
+
+},
+{
+    "id": 3,
+    "idCategoria": "elementos",
+    "nombre": "Cintos",
+    "descripcion": "Elemento escencial para el correcto armado de posturas.",
+    "precio": 1500,
+    "stock": 15,
+    "img": "img/cinto.jpg"
+
+},
+{
+    "id": 4,
+    "idCategoria": "meditación",
+    "nombre": "Zafu De Meditación",
+    "descripcion": "Zafu de meditación 16 X 32 Cm. Doble funda. Relleno de cáscara de trigo.",
+    "precio": 3500,
+    "stock": 10,
+    "img": "img/zafu.jpg"
+
+},
+{
+    "id": 5,
+    "idCategoria": "elementos",
+    "nombre": "Esfera",
+    "descripcion": "Esfera para trabajar distintos planos del cuerpo de forma segura.",
+    "precio": 3500,
+    "stock": 8,
+    "img": "img/esfera.jpg"
+
+},
+{
+    "id": 6,
+    "idCategoria": "meditación",
+    "nombre": "Kit Meditación",
+    "descripcion": "Kit de almohadilla para ojos, almohada cervical y almohadilla extra. Rellenas de semillas y lavanda.",
+    "precio": 2800,
+    "stock": 12,
+    "img": "img/kit.jpg"
+
+}
+]
+
+export default products;
